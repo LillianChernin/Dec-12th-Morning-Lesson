117fbbad4cc4af97ad640e43905a2f45d9bf509c
Lillian Chernin <l.smith1654@gmail.com>
2017-12-12 11:34:26 -0600

    answer final activity

--- objects2.js
+++ objects2.js
@@ -56,3 +56,68 @@ adventurer.companion2 = {
 console.log(adventurer.companion2)
 
 // * 6 minutes
+
+const adventurer2 = {
+	name: "Timothy",
+	hitpoints: 10,
+	belongings: ["sword", "potion", "Tums"],
+	companion: {
+	             name: "Velma",
+	             type: "Bat",
+	             companion: {
+	             					name: "Tim",
+	             					type: "Parasite",
+	             					belongings: ["SCUBA tank", "Rogan josh", "health insurance"]	                        }
+	           }
+}
+
+// console.log(adventurer2[companion]companion.type);
+// console.log(adventurer2.companion.companion.belongings[2]);
+
+for (let key in adventurer2) {
+	console.log(adventurer2[key]);
+}
+
+
+const movie = { title: "Eraserhead", director: "David Lynch", year: 1978 }
+
+
+// * Use a **for..in** loop to print all the keys of the movie object
+// * Use a **for..in** loop to print all the values of the movie object
+// * 7 minutes
+
+for (let key in movie) {
+  console.log(key);
+}
+
+for (let key in movie) {
+  console.log(movie[key]);
+}
+
+
+const movies = [
+               { title: "L'Avventura", director: "Michelangelo Antonioni", year: 1960 },
+               { title: "Eraserhead", director: "David Lynch", year: 1978 },
+               { title: "Dayereh", director: "Jafar Panahi", year: 2000 },
+               { title: "Dayereh", director: "Jafar Panahi", year: 2000 }
+             ]
+
+
+// Create an empty object. Leverage the fact that Keys are unique in an object. Loop over the movies array and add the movie titles as keys in the object. This will consequently omit the duplicate movie. At the same time, give each a value of **0** in the object.
+//
+// _Tip:_ You might want to use this technique for the hardest part of tonight's homework.
+//
+// * Use **Object.keys** loop to print all the values of the movie object.
+// * 10 minutes
+
+const movies2 = {}
+
+for (let i = 0; i < movies.length; i++) {
+  movies2[movies[i].title] = 0;
+}
+
+const keysOfMovies2 = Object.keys(movies2);
+
+for (let i = 0; i < keysOfMovies2.length; i++) {
+  console.log(movies2[keysOfMovies2[1]]);
+}
